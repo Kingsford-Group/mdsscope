ff403878ceac1869006c1fbeb167310d9c9a6a05
Guillaume Marçais <gmarcais@cs.cmu.edu>
2024-08-22 19:04:39 -0400

    add missing typename.hpp

---
+++ typename.hpp
@@ -0,0 +1,29 @@
+#ifndef TYPENAME_H_
+#define TYPENAME_H_
+
+#include <string_view>
+
+template <typename T>
+constexpr auto type_name() {
+  std::string_view name, prefix, suffix;
+#ifdef __clang__
+  name = __PRETTY_FUNCTION__;
+  prefix = "auto type_name() [T = ";
+  suffix = "]";
+#elif defined(__GNUC__)
+  name = __PRETTY_FUNCTION__;
+  prefix = "constexpr auto type_name() [with T = ";
+  suffix = "]";
+#elif defined(_MSC_VER)
+  name = __FUNCSIG__;
+  prefix = "auto __cdecl type_name<";
+  suffix = ">(void)";
+#endif
+  name.remove_prefix(prefix.size());
+  name.remove_suffix(suffix.size());
+  return name;
+}
+
+#define nameof(x) (type_name<x>())
+
+#endif // TYPENAME_H_
